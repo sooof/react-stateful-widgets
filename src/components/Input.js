2c3c69ce16a9f8200c2258fa9132ce699c732402
sooof <sooof@me.com>
2021-10-04 21:27:05 -0700

    finish Spinner.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -34,11 +34,17 @@ STEP 6:
   We need to add an extra prop to the <input /> element like so: value={inputValue}
 */
 
-import React from 'react'; /* STEP 0 */
+import React,{ useState } from 'react'; /* STEP 0 */
 
 export default function Input() {
   /* STEP 1 */
-
+  const [inputValue, setInputValue] = useState('t');
+  // console.log(inputValue);
+  // console.log(inputValue.length);
+  /* STEP 2 */
+  if(inputValue.length > 10){
+  //  console.log('In if')
+  }
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
